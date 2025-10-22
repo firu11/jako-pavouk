1b6fb8a78bb7a69d0258a915a0d4abc88a1d82d5
firu11 <firu@firu.dev>
2025-10-22 12:15:16 +0200

    refactor + new tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,18 +2,13 @@ import re
 from playwright.sync_api import Page, expect
 
 
-def test_has_title(page: Page):
-    _ = page.goto("/")
-
-    # Expect a title "to contain" a substring.
+def test_ma_h1(page: Page):
+    page.goto("/")
     expect(page).to_have_title(re.compile("Jako Pavouk"))
 
 
-def test_get_started_link(page: Page):
-    _ = page.goto("/")
+def test_zacit_psat(page: Page):
+    page.goto("/")
 
-    # Click the get started link.
     page.get_by_role("button", name="Začít psát").click()
-
-    # Expects page to have a heading with the name of Installation.
     expect(page.get_by_role("heading", name="První krůčky")).to_be_visible()
